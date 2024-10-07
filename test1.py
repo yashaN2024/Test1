a156258241893f792bb8d1111b27bd672b3c6314
yashaN2024 <zaitsewaanastasia@gmail.com>
2024-10-07 20:43:22 +0700

    Update test1.py
    подтвердил 1 раз

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
 from datetime import datetime
+
+print ("Hello АКАДЕМИЯ ТОП")
+date_birth = input() #"11.10.2007"
+print(datetime.date(datetime.strptime(date_birth,"%Y-%m-%d")))
+date_birth_data = datetime.date(datetime.strptime(date_birth,"%Y-%m-%d"))
+year = date_birth_data.year
+print(year)
+now_year = datetime.now().year
+#print(now_year)
+#age = now_year - year
+#print(age)
+a =  "Второй Урок"
+b = 10
+print(a * b)
+#
+print(a)
