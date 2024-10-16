b182ce9b6fbb77b318804796859b95a5646736eb
yashaN2024 <zaitsewaanastasia@gmail.com>
2024-10-16 21:00:06 +0700

    16.10.24

---
+++ test8.py
@@ -0,0 +1,16 @@
+string = input().split(" ")
+number = []
+print(string)
+for i in string:
+    zp = int(i)
+    print(volume)
+    if 0 < volume < 500:
+        zp *= 1.03
+    elif 500 <= volume <= 1000:
+        zp *= 1.05
+
+
+
+
+
+
