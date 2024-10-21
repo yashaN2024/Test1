5ee0e0d2e311e2f0943eb2d3d6cccee504d311fb
yashaN2024 <zaitsewaanastasia@gmail.com>
2024-10-21 20:50:33 +0700

    21.10.24

---
+++ test9.py
@@ -0,0 +1,38 @@
+a1 = 1
+a2 = 2
+a3 = 4
+a4 = 6
+a = ["sgfgs", "rgfgsf", "sgss", "srgs"]
+b = a[0] + a[1]
+print(b)
+# b.insert
+# print(b)
+
+#         list2.append(1)
+# print(list2)
+
+customers=[1,7,3,2,10]
+
+list2 = [i for i in customers]
+
+print(sum(customers)) # to do
+result = 1
+for i in customers:
+    print(i)
+    result *= i
+print(result)
+
+string = input().split(" ")
+
+even = 0
+odds = 0
+int9 = 0
+for i in range(int(string[0], int(string[1]))):
+    if i % 2 ==0:
+        even += 1
+        even_numbers.append(i)
+    if i % 2 !=0:
+        odds += 1
+        odds_numbers.append(i)
+
+
