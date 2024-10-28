7df62013852c41d656cb49e73306add64c6c350e
yashaN2024 <zaitsewaanastasia@gmail.com>
2024-10-28 20:40:38 +0700

    28.10.24

---
+++ test11.py
@@ -0,0 +1,55 @@
+result = ""
+for i in range(100, 1000):
+    s = str(i)
+    #print(s)
+    #num = []
+    #for j in s:
+#         num.append(j)
+#    if num[0] == num[1] or num[1] == num[2] or num[0] == num[2]:
+#        if num[0] != num[1] != num[2]
+#        result.append(int(num[0]+num[1]+num[2]))
+#print(len(result))
+
+
+s = input().split(" ")
+numbers =[]
+for i in range(int(s[0]), int(s[1]) + 1):
+    num = []
+    for x in str(i):
+        num.append(x)
+    for j in range(10):
+        if num.count(str(j))
+
+
+#for i in range(31):
+#    print(" " * i, "*" * (31 - i * 2 ))
+
+i = 0
+while i < 31:
+    print(" " * (30+i), "*" * (31 - i * 2))
+    i += 1
+
+s = eval(input())
+
+s = input().lower().replace(  " ",   " ")
+print(s)
+reverse_s = s[::-1]
+print(reverse_s)
+if s == reverse_s:
+
+s1 = input().split(" ")
+s2 = input().split(" ")
+#numbers = []
+print(numbers1 := max([int(s) for s in s1]),
+      min([int(s) for s in s1]))
+
+s = input().replace(   "3",   "",   10).replace(   "6",)
+
+#num = ""
+#for i in s:
+#    if i != "3" and i != "6":
+#        num += i
+#print(int(num))
+
+
+
