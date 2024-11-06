1eae2364af830322663cf6ca1bc91ca5c20c1384
yashaN2024 <zaitsewaanastasia@gmail.com>
2024-11-06 20:56:07 +0700

    06.11.24

---
+++ test13.py
@@ -0,0 +1,82 @@
+
+
+
+
+while i < 40:
+    if i <=20:
+print(' '*19, '_'*45)
+i = 1
+
+
+
+    # print(" "*20, "|" +
+    #      "*" * i if i <= 20 else "*" * (40 - i),
+    #     "x" * (40 - i) if i <=20 else "x" * i,
+    #     "|")
+    i += 1
+print(' '*19, '-'*45)
+
+spisok = [-1, -2, -4, -5, 0, 3, 3, 5, 6, 63, 3, 4]
+positive = 0
+negative = 0
+nulls = 0
+for x in spisok:
+    if x < 0:
+        negative += 1
+    if x > 0:
+        positive += 1
+    if x ==0:
+        nulls += 1
+print(max(spisok))
+print(min(spisok))
+print(positive, negative, nulls)
+
+import re
+s = input()
+operation = re.findall(     r'[(\-+*)]', s)
+print(operation)
+numbers = re.split(      r'[(\-/\+*)]', s)
+print(numbers)
+j = 1
+result = 0
+
+A = "1111"
+ZARPATA = 1000
+
+def test13(s = "", a, b):
+    if not s: s = "Пустая строка"
+    d = ZARPATA
+    print(d)
+    return
+
+result, mulab = test13("2", 5)
+print(result, mulab)
+
+j = 0
+res.clear()
+if operation
+res.append(int(numbers[j]) * (int(numbers))
+
+
+print("1",numbers)
+
+print("2",numbers)
+
+
+
+
+
+operation.pop(j)
+print("4", operation)
+j = 0
+   #res.clear()
+   #10 * 4 - 11 * 5 + 12 * 6
+
+  j += 1
+
+
+
+
+
+
+
